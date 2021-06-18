ffaad2b901b64d5e87e3771d473dd806309f1b31
Apple <apple@Apples-Mac-mini.local>
2021-06-18 18:13:33 +0530

    initial

---
+++ screens/Place.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    ImageBackground
+} from 'react-native';
+
+
+const Place = ({navigation, route}) => {
+
+   const [selectedPlace, setSelectedPlace] = React.useState(null)
+    React.useEffect(()=>{
+        let {selectedPlace} = route.params;
+        setSelectedPlace(selectedPlace)
+    },[])
+
+    function renderPlace(){
+        return(
+            <ImageBackground
+             source={selectedPlace?.image}
+             style={{
+                 width:'100%',
+                 height:'100%'
+             }}
+            >
+            </ImageBackground>
+        )
+    }
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+           {renderPlace()}
+        </View>
+    )
+}
+
+export default Place;
